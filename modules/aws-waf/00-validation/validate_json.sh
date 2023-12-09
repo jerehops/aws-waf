dd293588b7c5f267b69ed82200001271720a94e7
Jeremy Ho <jerehops@gmail.com>
2023-12-09 23:47:59 +0800

    update validation module

--- modules/aws-waf/00-validation/validate_json.sh
+++ modules/aws-waf/00-validation/validate_json.sh
@@ -2,26 +2,21 @@
 
 echo "Rule Group Statement Length Checks"
 echo "================================="
-echo rule_group_statement_length_check = $rule_group_statement_length_check
+echo statement_length_check = $statement_length_check
 echo
 echo "Rule Group Statement Checks"
 echo "================================="
-echo rule_group_statement_check = $rule_group_statement_check
-echo
-echo "WebACL Statement Checks"
-echo "================================="
-echo webacl_statement_check = $webacl_statement_check
+echo statement_check = $statement_check
 echo
 
-IFS=',' rule_group_statement_length_check=( $rule_group_statement_length_check )
-IFS=',' rule_group_statement_check=( $rule_group_statement_check )
-IFS=',' webacl_statement_check=( $webacl_statement_check )
+IFS=',' statement_length_check=( $statement_length_check )
+IFS=',' statement_check=( $statement_check )
 
-if [[ $rule_group_statement_length_check != "" ]]; then
-  for ((idx=0; idx<${#rule_group_statement_length_check[@]}; ++idx)); do
-    rule_group_statement_length_check_values=${rule_group_statement_length_check[idx]}
+if [[ $statement_length_check != "" ]]; then
+  for ((idx=0; idx<${#statement_length_check[@]}; ++idx)); do
+    statement_length_check_values=${statement_length_check[idx]}
     IFS=' '
-    read -ra values <<< "$rule_group_statement_length_check_values"
+    read -ra values <<< "$statement_length_check_values"
     if [[ ("${values[0]}" == "match" || "${values[0]}" == "not") && "${values[1]}" > 1 ]];
     then
       echo "Error: Only 1 statement allowed for type Match and Not"
@@ -30,25 +25,14 @@ if [[ $rule_group_statement_length_check != "" ]]; then
   done
 fi
 
-if [[ $rule_group_statement_check != "" ]]; then
-  for ((idx=0; idx<${#rule_group_statement_check[@]}; ++idx)); do
-    rule_group_statement_check_values=${rule_group_statement_check[idx]}
-    read -ra values <<< "$rule_group_statement_check_values"
+if [[ $statement_check != "" ]]; then
+  for ((idx=0; idx<${#statement_check[@]}; ++idx)); do
+    statement_check_values=${statement_check[idx]}
+    read -ra values <<< "$statement_check_values"
     if [[ "${values[@]}" != "geo_match_statement" && "${values[@]}" != "ip_set_reference_statement" && "${values[@]}" != "regex_pattern_set_reference_statement" ]];
     then
-      echo "Error: Only geo_match_statement, ip_set_reference_statement, regex_pattern_set_reference_statement"
+      echo "Error: Only geo_match_statement, ip_set_reference_statement, regex_pattern_set_reference_statement, rule_group_reference_statement, managed_rule_group_statement allowed"
       exit 1
     fi
   done
 fi
-
-for ((idx=0; idx<${#webacl_statement_check[@]}; ++idx));
-do
-  webacl_statement_check_values=${webacl_statement_check[idx]}
-  read -ra values <<< "$webacl_statement_check_values"
-  if [[ "${values[@]}" != "rule_group_reference_statement" && "${values[@]}" != "managed_rule_group_statement" ]];
-  then
-    echo "Error: Only rule_group_reference_statement,managed_rule_group_statement allowed"
-    exit 1
-  fi
-done
--- modules/aws-waf/00-validation/validation.tf
+++ modules/aws-waf/00-validation/validation.tf
@@ -2,9 +2,8 @@ resource null_resource "validate_json" {
   provisioner "local-exec" {
     command = "chmod +x ./${path.module}/validate_json.sh && ./${path.module}/validate_json.sh"
     environment = {
-      rule_group_statement_length_check = join(",", local.rule_group_statement_length_check)
-      rule_group_statement_check = join(",", local.rule_group_statement_check)
-      webacl_statement_check = join(",", local.webacl_statement_check)
+      statement_length_check = join(",", local.statement_length_check)
+      statement_check = join(",", local.statement_check)
     }
   }
 
@@ -24,15 +23,11 @@ output "result" {
 
 
 locals {
-  rulegroup_file = try(jsondecode(file(var.rulegroup_file)), [])
   webacl_file    = jsondecode(file(var.webacl_file))
-  rule_group_statement_length_check = flatten([
-    for item in local.rulegroup_file : "${item.type} ${length(item.statement)}"
+  statement_length_check = flatten([
+    for item in local.webacl_file : "${item.type} ${length(item.statement)}"
   ])
-  rule_group_statement_check = flatten([
-    for v in local.rulegroup_file : keys(v.statement)
-  ])
-  webacl_statement_check = flatten([
+  statement_check = flatten([
     for v in local.webacl_file : keys(v.statement)
   ])
 }
